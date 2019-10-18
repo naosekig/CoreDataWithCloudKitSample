69f3fd9c14b97ec1f62e94bf70057c319407bc31
Naoaki Sekiguchi <nao.sekig@gmail.com>
2019-10-18 13:22:17 +0900

    Initial Commit

---
+++ CoreDataWithCloudKitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataWithCloudKitSample
+//
+//  Created by NAOAKI SEKIGUCHI on 2019/10/18.
+//  Copyright © 2019 NAOAKI SEKIGUCHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
